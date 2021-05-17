57d0201ec6ba029eae5caacdd24379157de1c530
Rodolfo Berrios <inbox@rodolfoberrios.com>
2021-05-17 18:59:19 -0400

    tags 2.2.1

--- installer.php
+++ installer.php
@@ -20,7 +20,7 @@
 declare(strict_types=1);
 
 const APP_NAME = 'Chevereto Installer';
-const APP_VERSION = '2.2.0';
+const APP_VERSION = '2.2.1';
 const APP_URL = 'https://github.com/chevereto/installer';
 const PHP_VERSION_MIN = '7.4';
 const PHP_VERSION_RECOMMENDED = '7.4';
